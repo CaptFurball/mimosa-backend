66bfb78eef24a0da20d89a189973691841afae0f
captfurball <elys.1993a@gmail.com>
2020-09-06 10:04:09 +0800

    Change dash to underscore in file name + add migratio for tags table

--- src/database/migrations/2020_09_06_015615_create_tags_table.php
+++ src/database/migrations/2020_09_06_015615_create_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStoriesTable extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateStoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('stories', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->foreignId('user_id')->onDelete('cascade');
-            $table->longText('body');
-            $table->text('tags')->nullable();
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateStoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('stories');
+        Schema::dropIfExists('tags');
     }
 }
